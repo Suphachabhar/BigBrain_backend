4b2ee88ed6742e2ce9f2723339a6d5ec96f6b4a9
Suphachahar <z5176892@ad.unsw.edu.au>
2021-06-01 15:38:58 +1000

    add files

---
+++ src/custom.js
@@ -0,0 +1,41 @@
+/*
+ For a given data structure of a question, produce another
+ object that doesn't contain any important meta data (e.g. the answer)
+ to return to a "player"
+*/
+export const quizQuestionPublicReturn = question => {
+  const output = {
+    description: question.description,
+    time: question.time,
+    point: question.point,
+    src: question.src,
+    choices: question.choices,
+    multiple: question.answers.length > 1
+  };
+  console.log('See question: ', output);
+  return output;
+};
+
+/*
+ For a given data structure of a question, get the IDs of
+ the correct answers (minimum 1).
+*/
+export const quizQuestionGetCorrectAnswers = question => {
+  return question.answers;
+};
+
+/*
+ For a given data structure of a question, get the IDs of
+ all of the answers, correct or incorrect.
+*/
+export const quizQuestionGetAnswers = question => {
+  return question.choices.map((choice) => { return choice.id; });
+};
+
+/*
+ For a given data structure of a question, get the duration
+ of the question once it starts. (Seconds)
+*/
+export const quizQuestionGetDuration = question => {
+  return question.time;
+};
